c986e1f67a3fd1c73428c1ef869bc9257a491bb5
starLLong <2554868271@qq.com>
2022-04-16 17:23:57 +0800

    更新创建

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import element from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css'
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(element).mount('#app')
